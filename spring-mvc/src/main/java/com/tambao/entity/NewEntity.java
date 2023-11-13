4c52182e82dc7c5ad9bf948a0f08dc18ad23e383
phamvantam <phamvantam1911@gmail.com>
2023-11-13 21:48:53 +0700

    config, ánh xạ, Jpa, Spring data JPA xuống csdl

---
+++ spring-mvc/src/main/java/com/tambao/entity/NewEntity.java
@@ -0,0 +1,65 @@
+package com.tambao.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "new")
+public class NewEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "title")
+	private String title;
+	
+	@Column(name = "thumnail")
+	private String thumnail;
+	
+	@Column(name = "shortdescription")
+	private String shortDescription;
+	
+	@Column(name = "content")
+	private String content;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getThumnail() {
+		return thumnail;
+	}
+
+	public void setThumnail(String thumnail) {
+		this.thumnail = thumnail;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
